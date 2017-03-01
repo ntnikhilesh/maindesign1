463f32796a84e41957521b58e2427f38b8dd6229
nikhil <ntnikhilesh@gmail.com>
2017-03-01 17:55:16 +0530

    main-tab-detail updated succ...

--- src/app/main-tab-detail/main-tab-detail.component.ts
+++ src/app/main-tab-detail/main-tab-detail.component.ts
@@ -2,21 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 import {Router} from '@angular/router';
 
+import {Weg} from './../models/weg';
+import {WegService} from './../services/weg.service';
+
 @Component({
   selector: 'app-main-tab-detail',
   templateUrl: './main-tab-detail.component.html',
   styleUrls: ['./main-tab-detail.component.css']
 })
 export class MainTabDetailComponent implements OnInit {
+weg:Weg[];
 
-  constructor(private router:Router) { }
+  //Consume the service
+  constructor(private wegService:WegService) { }
 
   ngOnInit() {
+  this.getWeg();
   }
 
-  goToDetail()
-  {
-  	this.router.navigate(['/help']);
-  }
+  getWeg():void {
+ this.wegService.getWeg().then(weg=>this.weg=weg);
+ }
 
 }
