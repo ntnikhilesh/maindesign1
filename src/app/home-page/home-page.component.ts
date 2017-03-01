98fb9634b426b8ef6562670ef388fb961566ad59
nikhil <ntnikhilesh@gmail.com>
2017-03-01 17:01:59 +0530

    tab item uploaded on home page  using service

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Weg} from './../models/weg';
+import {WegService} from './../services/weg.service';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+weg:Weg[];
 
-  constructor() { }
+//Consume the service
+  constructor(private wegService:WegService) { }
 
   ngOnInit() {
+  this.getWeg();
   }
+
+  getWeg():void {
+ this.wegService.getWeg().then(weg=>this.weg=weg);
+ }
 
 }
