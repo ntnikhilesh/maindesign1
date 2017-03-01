a61f50e41ea92bcae2f3bc474a07a7df6641ff4d
nikhil <ntnikhilesh@gmail.com>
2017-03-01 06:47:48 +0530

    card size updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+export class Weg {
+  img:string;
+  name: string;
+}
+const WEG: Weg[] = [
+  { img:'http://vegetablebag.com/wp-content/uploads/2016/08/thor.png', name: 'Banana Steam' },
+  { img:'https://3.imimg.com/data3/NV/FC/MY-10077549/fresh-red-pumpkin-500x500.jpg', name: 'Red Pumpkin' },
+  { img:'http://vegetablebag.com/wp-content/uploads/2016/08/thor.png', name: 'Banana Steam' },
+  { img:'https://3.imimg.com/data3/NV/FC/MY-10077549/fresh-red-pumpkin-500x500.jpg', name: 'Red Pumpkin' },
+  { img:'http://vegetablebag.com/wp-content/uploads/2016/08/thor.png', name: 'Banana Steam' },
+  { img:'https://3.imimg.com/data3/NV/FC/MY-10077549/fresh-red-pumpkin-500x500.jpg', name: 'Red Pumpkin' },
+  { img:'http://vegetablebag.com/wp-content/uploads/2016/08/thor.png', name: 'Banana Steam' },
+  { img:'https://3.imimg.com/data3/NV/FC/MY-10077549/fresh-red-pumpkin-500x500.jpg', name: 'Red Pumpkin' }
+
+  
+];
+
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.dcomponent.html',
-  styleUrls: ['./app.dcomponent.css']
+  templateUrl: './app.ecomponent.html',
+  styleUrls: ['./app.ecomponent.css']
 })
 export class AppComponent {
   title = 'Hello Ravi';
+weg = WEG;
+
 }
