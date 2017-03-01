fd776c67d3b03bb255df03d60d2c9e990c13a044
nikhil <ntnikhilesh@gmail.com>
2017-03-01 19:53:21 +0530

    veg pic updated

--- src/app/models/mock-weg.ts
+++ src/app/models/mock-weg.ts
@@ -1,13 +1,37 @@
 import {Weg} from './weg'
 export const WEG: Weg[] = [
-  { img:'http://vegetablebag.com/wp-content/uploads/2016/08/thor.png', name: 'Banana Steam' },
-  { img:'https://3.imimg.com/data3/NV/FC/MY-10077549/fresh-red-pumpkin-500x500.jpg', name: 'Red Pumpkin' },
-  { img:'http://vegetablebag.com/wp-content/uploads/2016/08/thor.png', name: 'Banana Steam' },
-  { img:'https://3.imimg.com/data3/NV/FC/MY-10077549/fresh-red-pumpkin-500x500.jpg', name: 'Red Pumpkin' },
-  { img:'http://vegetablebag.com/wp-content/uploads/2016/08/thor.png', name: 'Banana Steam' },
-  { img:'https://3.imimg.com/data3/NV/FC/MY-10077549/fresh-red-pumpkin-500x500.jpg', name: 'Red Pumpkin' },
-  { img:'http://vegetablebag.com/wp-content/uploads/2016/08/thor.png', name: 'Banana Steam' },
-  { img:'https://3.imimg.com/data3/NV/FC/MY-10077549/fresh-red-pumpkin-500x500.jpg', name: 'Red Pumpkin' }
+  { img:'http://i.imgur.com/qQlNx4d.jpg', name: 'Avarekayi' },
+  { img:'http://i.imgur.com/cDIdHUt.jpg', name: 'Beans' },
+  { img:'http://i.imgur.com/lDZRh55.jpg', name: 'Beetroot' },
+  { img:'http://i.imgur.com/4dyI8uJ.jpg', name: 'Bottle Gourd' },
+  { img:'http://i.imgur.com/uA73YLa.jpg', name: 'Carrot' },
+  { img:'http://i.imgur.com/4PJnaH3.jpg', name: 'Chayote' },
+  { img:'http://i.imgur.com/r83cYGS.jpg', name: 'Coriander' },
+  { img:'http://i.imgur.com/8KvNAsz.jpg', name: 'Curry Leaf' },
+  { img:'http://i.imgur.com/NChRfPJ.jpg', name: 'Double Beans' },
+  { img:'http://i.imgur.com/p9q0YZJ.jpg', name: 'Drumstic' },
+  { img:'http://i.imgur.com/98nG7qm.jpg', name: 'Egg' },
+  { img:'http://i.imgur.com/nyqauzv.jpg', name: 'Fenu Greek Leaves' },
+  { img:'http://i.imgur.com/34x06jr.jpg', name: 'Garlic' },
+  { img:'http://i.imgur.com/67wy2iX.jpg', name: 'Gobi' },
+  { img:'http://i.imgur.com/MHKQsMy.jpg', name: 'Green Chilli' },
+  { img:'http://i.imgur.com/p3GH77D.jpg', name: 'Knol Khol' },
+  { img:'http://i.imgur.com/MBaxWTU.jpg', name: 'Ladies Finger' },
+  { img:'http://i.imgur.com/OIa7g7T.jpg', name: 'Lemon' },
+  { img:'http://i.imgur.com/uKJDwjL.jpg', name: 'Mushroom' },
+  { img:'http://i.imgur.com/6yum6t7.jpg', name: 'Onion' },
+  { img:'http://i.imgur.com/Ax0RuFo.jpg', name: 'Peas' },
+  { img:'http://i.imgur.com/IJEMK2s.jpg', name: 'Potato' },
+  { img:'http://i.imgur.com/tCzT16L.jpg', name: 'Pudina' },
+  { img:'http://i.imgur.com/q8183Kc.jpg', name: 'Raddish' },
+  { img:'http://imgur.com/1SqfDa0.jpg', name: 'Ridge Gourd' },
+  { img:'http://i.imgur.com/TnEF8I0.jpg', name: 'Snake Gourd' },
+  { img:'http://i.imgur.com/qtHb7Hn.jpg', name: 'Suvarna Gedde' },
+  { img:'http://i.imgur.com/cBZ7nBK.jpg', name: 'Thonde Kayi' },
+  { img:'http://i.imgur.com/kmw3vom.jpg', name: 'Tomato' }
+  
+
+
 
   
 ];
