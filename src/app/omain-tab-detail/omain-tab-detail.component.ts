40af00ae5170eb2ecef42d5d6df06179d4613694
nikhil <ntnikhilesh@gmail.com>
2017-03-14 09:46:14 +0530

    service worker added

--- src/app/omain-tab-detail/omain-tab-detail.component.ts
+++ src/app/omain-tab-detail/omain-tab-detail.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { AfoListObservable, AngularFireOffline } from 'angularfire2-offline';
 import {FirebaseService} from './../services/firebase.service';
 
+import * as firebase from 'firebase';
+
 @Component({
   selector: 'app-omain-tab-detail',
   templateUrl: './omain-tab-detail.component.html',
@@ -10,15 +12,35 @@ import {FirebaseService} from './../services/firebase.service';
 })
 export class OmainTabDetailComponent implements OnInit {
 
-  
+  	
   	olistings:any;
+  	imageUrl:any;
 
   constructor(private firebaseService:FirebaseService) { }
 
   ngOnInit() {
   this.firebaseService.getafoListings().subscribe(listings=>{
-  console.log(listings);
+  //console.log();
+  
+  this.getImage();
   this.olistings=listings;
+  //console.log(key);
   }); 
+
+
+  //get image URL
+
+
+
+  }
+
+
+  getImage()
+  {
+  console.log('your image path');
+
+
+
+
   }
 }
